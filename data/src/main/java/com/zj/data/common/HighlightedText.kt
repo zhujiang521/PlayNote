d23cc550085e5820d22aa8175d6b0b5a56791fc2
朱江 <zhujiang2@lenovo.com>
2025-07-17 15:28:22 +0800

    增加注释

--- data/src/main/java/com/zj/data/common/HighlightedText.kt
+++ data/src/main/java/com/zj/data/common/HighlightedText.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -12,6 +11,15 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import com.zj.data.R
 
+/**
+ * 显示带有高亮的文本
+ *
+ * @param modifier UI 修改器，用于定义组件的布局属性
+ * @param text 要显示的原始文本
+ * @param highlight 需要高亮的关键词
+ * @param style 文本样式，默认为 MaterialTheme.typography.bodyMedium
+ * @param maxLines 文本显示的最大行数，默认为 Int.MAX_VALUE
+ */
 @Composable
 fun HighlightedText(
     modifier: Modifier = Modifier,
